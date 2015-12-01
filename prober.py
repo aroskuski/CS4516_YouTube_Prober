cc7e24e2ea8b4b40170038f09040019ddff2f4d9
Andrew Roskuski <aroskuski@gmail.com>
2015-11-30 20:25:41 -0500

    Eliminated duplicate prefixes

--- prober.py
+++ prober.py
@@ -13,19 +13,31 @@ DEVELOPER_KEY = "REPLACE_ME"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-
+charSet = string.ascii_letters + string.digits + '_'
 
 def prefix_generator(length, amount):
-	charSet = string.ascii_letters + string.digits + '_'
-	usedprefixes = []
+	
+	usedprefixes = ['']
 	for i in range(0, amount):
-		prefix = ''
-		for j in range(0, length):
-			prefix = prefix + charSet[random.randint(0,len(charSet) - 1)]
+		prefix = gen_prefix(length)
+		
+		while prefix in usedprefixes:
+			prefix = gen_prefix(length)
+
+		usedprefixes.append(prefix)
+		
 		yield prefix
+
+def gen_prefix(length):
+	prefix = ''
+	global charSet
+	for i in range(0, length):
+		prefix = prefix + random.choice(charSet)
+	return prefix
 
 
 def youtube_search(prefix):
+	query = 'watch?v=' + prefix
 	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
 	search_response = youtube.search().list(q=query, part="id,snippet", maxResults=50).execute()
@@ -35,7 +47,7 @@ def youtube_search(prefix):
 	for search_result in search_response.get("items", []):
 		if search_result["id"]["kind"] == "youtube#video":
 			if search_result["id"]["videoId"].startswith(prefix):
-				pass
+				good_results.append(search_result)
 	
 
 def prefix_search():
