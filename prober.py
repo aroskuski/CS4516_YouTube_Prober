09926c8bf8fe8c623de6d6ca8d7d097109cd3d2c
Andrew Roskuski <aroskuski@gmail.com>
2015-12-05 20:56:54 -0500

    Wrote some more code. Made note of the major unfinished tasks

--- prober.py
+++ prober.py
@@ -13,6 +13,8 @@ DEVELOPER_KEY = "REPLACE_ME"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
 charSet = string.ascii_letters + string.digits + '_'
 
 def prefix_generator(length, amount):
@@ -38,18 +40,29 @@ def gen_prefix(length):
 
 def youtube_search(prefix):
 	query = 'watch?v=' + prefix
-	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-
+	global youtube
 	search_response = youtube.search().list(q=query, part="id,snippet", maxResults=50).execute()
 
 	good_results = []
 
+	#TODO: handle pagination
+
 	for search_result in search_response.get("items", []):
 		if search_result["id"]["kind"] == "youtube#video":
 			if search_result["id"]["videoId"].startswith(prefix):
-				good_results.append(search_result)
+				good_results.append(search_result["id"]["videoId"])
+
+	return good_results
 	
 
-def prefix_search():
-	pass
+def prefix_search(length, amount):
+	results = []
+	for i in prefix_generator(length, amount):
+		results.append(youtube_search(i))
+
+def get_video_details(video_id):
+	global youtube
+	video_details = youtube.videos.list(part="id,snippet",id=video_id).exectute()
+	#TODO: Process details into desired format
+	return video_details
 
