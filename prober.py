d2dde88f2be62b095b1807d2c7371c95f376b5a6
Alec Benson <abenson@wpi.edu>
2015-12-07 20:06:05 -0500

    Broke up method

--- prober.py
+++ prober.py
@@ -13,72 +13,74 @@ DEVELOPER_KEY = "REPLACE_ME"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                developerKey=DEVELOPER_KEY)
 
 charSet = string.ascii_letters + string.digits + '_'
 
-def prefix_generator(length, amount):	
-	usedprefixes = ['']
-	for i in range(0, amount):
-		prefix = gen_prefix(length)
-		
-		while prefix in usedprefixes:
-			prefix = gen_prefix(length)
 
-		usedprefixes.append(prefix)
-		
-		yield prefix
+def prefix_generator(length, amount):
+    usedprefixes = ['']
+    for i in range(0, amount):
+        prefix = gen_prefix(length)
+
+        while prefix in usedprefixes:
+            prefix = gen_prefix(length)
+
+        usedprefixes.append(prefix)
+
+        yield prefix
+
 
 def gen_prefix(length):
-	prefix = ''
-	global charSet
-	for i in range(0, length):
-		prefix = prefix + random.choice(charSet)
-	return prefix
+    prefix = ''
+    global charSet
+    for i in range(0, length):
+        prefix = prefix + random.choice(charSet)
+    return prefix
+
+
+def get_matches(prefix, search_response):
+    good_results = []
+    for search_response in search_response.get("items", []):
+        if search_response["id"]["kind"] == "youtube#video":
+            if search_response["id"]["videoId"].startswith(prefix):
+                good_results.append(search_response["id"]["videoId"])
+    return good_results
 
 
 def youtube_search(prefix):
-	query = 'watch?v=' + prefix
-	global youtube
-	print(prefix)
-	search_response = youtube.search().list(q=query, part="id,snippet", maxResults=50).execute()      
-	good_results = []
+    query = 'watch?v=' + prefix
+    global youtube
+    good_results = []
 
-	#TODO: handle pagination
-	nextPageToken = search_response.get("nextPageToken")
+    search_response = youtube.search().list(
+        q=query, part="id,snippet", maxResults=50).execute()
+    npToken = search_response.get("nextPageToken")
+    good_results.extend(get_matches(prefix, search_response))
 
-	for search_result in search_response.get("items", []):
-		if search_result["id"]["kind"] == "youtube#video":
-			if search_result["id"]["videoId"].startswith(prefix):
-				good_results.append(search_result["id"]["videoId"])
+    while npToken != None:
+        search_response = youtube.search().list(
+            q=query, part="id,snippet", maxResults=50, pageToken=npToken).execute()
+        npToken = search_response.get("nextPageToken")
+        good_results.extend(get_matches(prefix, search_response))
 
-	# Look at all of the pages
-	while (nextPageToken != None):
-		print(nextPageToken)
-		search_response = youtube.search().list(q=query, part="id,snippet", maxResults=50, pageToken=nextPageToken).execute()      
+    return good_results
 
-		nextPageToken = search_response.get("nextPageToken")
-
-		for search_result in search_response.get("items", []):
-			if search_result["id"]["kind"] == "youtube#video":
-				if search_result["id"]["videoId"].startswith(prefix):
-					good_results.append(search_result["id"]["videoId"])
-	return good_results
-	
 
 def prefix_search(length, amount):
-	results = []
-	for i in prefix_generator(length, amount):
-		results += youtube_search(i)
-	print(results)
-	detailedResults = [get_video_details(x) for x in results]	
-	outfile = open('res.txt','w')
-	outfile.write(json.dumps(detailedResults))
-	outfile.close()
+    results = []
+    for i in prefix_generator(length, amount):
+        results += youtube_search(i)
+    print(results)
+    detailedResults = [get_video_details(x) for x in results]
+    outfile = open('res.txt', 'w')
+    outfile.write(json.dumps(detailedResults))
+    outfile.close()
 
 
 def get_video_details(video_id):
-	global youtube
-	video_details = youtube.videos().list(part="id,snippet,contentDetails,status,statistics",id=video_id).execute()
-	return video_details
-
+    global youtube
+    video_details = youtube.videos().list(
+        part="id,snippet,contentDetails,status,statistics", id=video_id).execute()
+    return video_details
